1be73e683476d0d606b18a589ea7160e7c59c4f2
Mohammad Javad Abolhassani Far <com.javadabl@gmail.com>
2025-11-03 14:44:38 +0330

    dev

--- src/banking/banking.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ReconciliationService } from './reconciliation.service';
-import { ReconciliationController } from './reconciliation.controller';
-
-@Module({
-  providers: [ReconciliationService],
-  controllers: [ReconciliationController]
-})
-export class BankingModule {}
--- src/general-ledger/general-ledger.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { JournalEntryService } from './journal-entry.service';
-import { AccountService } from './account.service';
-
-@Module({
-  providers: [JournalEntryService, AccountService]
-})
-export class GeneralLedgerModule {}
